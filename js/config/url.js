2c8d564e903441ed6386e384f44813dab1f602de
Febriand1 <fdirga63@gmail.com>
2023-04-01 05:09:34 +0700

    budiman frontend

---
+++ js/config/url.js
@@ -0,0 +1,1 @@
+export let urlAPI = "https://ws-nilai.herokuapp.com/nilai";
---
+++ js/controller/get.js
@@ -0,0 +1,23 @@
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "../temp/table.js";
+export function isiTablePresensi(results) {
+  results.forEach(isiRow);
+}
+function isiRow(value) {
+  let content = isiTabel
+    .replace("#NPM#", value.absensi.biodata.npm)
+    .replace("#NAMA#", value.absensi.biodata.nama)
+    .replace("#MATAKULIAH#", value.kategori.nama_mk)
+    .replace("#TUGAS1#", value.alltugas.tugas1)
+    .replace("#TUGAS2#", value.alltugas.tugas2)
+    .replace("#TUGAS3#", value.alltugas.tugas3)
+    .replace("#TUGAS4#", value.alltugas.tugas4)
+    .replace("#TUGAS5#", value.alltugas.tugas5)
+    .replace("#UTS#", value.uts)
+    .replace("#UAS#", value.uts)
+    .replace("#GRADE#", value.grade.namagrade)
+    .replace("#WARNA#", getRandomColor())
+    .replace(/#WARNALOGO#/g, getRandomColorName());
+  addInner("iniTabel", content);
+}
